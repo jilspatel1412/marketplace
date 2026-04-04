896058222816095d6e9aae5be95e5a609c7fe26b
jilspatel1412 <jilspatel141204@gmail.com>
2026-04-04 18:36:01 -0400

    One-time cleanup: delete all users except ADMINJILS

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py create_admin
+
+# One-time: delete all users except ADMINJILS and mark as verified
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+User.objects.exclude(username='ADMINJILS').delete()
+u = User.objects.get(username='ADMINJILS')
+u.is_verified = True
+u.save()
+print('Cleanup done. Remaining users:', list(User.objects.values_list('username', flat=True)))
+"
